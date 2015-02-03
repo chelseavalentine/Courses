ee7f6d525d3dda85c228e05d7515de96e56dc5f7
Chelsea <cv851@nyu.edu>
2015-02-03 15:18:22 -0500

    Create [3]Bonus.java

---
+++ [3]Bonus.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/*
+ * @title: Highest bonus
+ * @name: Chelsea Valentine
+ * 
+ * Assignment:
+ * 
+ * Write a program that prompts the user first to enter the number of employees and then to enter the name and bonus amount for each
+ * employee (the name should be a single string with no spaces, the bonus should be a real number).
+ * 
+ * The program should then display the name and bonus amount for the employee with the highest bonus.
+ * 
+ * Extra credit (5 points): Modify the above program to display two employees with the highest bonus amount.
+ * 
+ * Note: You are not allowed to use arrays, ArrayLists, etc.
+ */
+public class Bonus {
+	public static void main(String[] args) {
+
+	}
+}
