d1e2d09c9d66a856d06ed125568fc1f4a5b44ccc
Chelsea <cv851@nyu.edu>
2015-02-03 15:12:03 -0500

    Create [2]Hexagon.java

---
+++ [2]Hexagon.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/* @title: Distance of two points
+ * @author: Chelsea Valentine
+ * 
+ * Assignment:
+ * 
+ * Write a program that prompts the user to enter the length of the side of a hexagon and computes its area and perimeter.
+ * The area of a hexagon with size x can be computed using:
+ * 
+ * Area = (6x^2)/(4tan(pi/6))
+ * 
+ * and the perimeter of a hexagon with size x can be computed using:
+ * 
+ * Perimeter = 6x.
+ * 
+ * Note 1: You can use Math.tan(a) to compute tan(a)
+ * Note 2: There's no exponentiation operator in Java. In order to square a number, you can either multiply it by itself (good solution),
+ * or use Math.pow(a, 2) (bad solution)
+ */
+
+public class Hexagon {
+	public static void main(String[] args) {
+		
+	}
+}
