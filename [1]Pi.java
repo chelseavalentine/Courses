670c5b516991a95182bc0ffbeb4635403d0d6efe
Chelesa Valentine <cv851@nyu.edu>
2015-02-03 16:12:36 -0500

    finish Pi.java assignment

--- [1]Pi.java
+++ [1]Pi.java
@@ -19,13 +19,45 @@ import java.util.*;
 public class Pi {
 	public static void main(String[] args) {
 		
+		//Define variables
+		double denominator = 1;
+		double sign = 1; //Variable to control whether the fraction is positive or negative
+		double answer1, answer2, answer3;
+		answer1 = answer2 = answer3 = 0;
+
 		//Calculate the answer to the first question: 4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11)
-		System.out.print("The answer to 4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11) is: \n\n");
+		System.out.print("The answer to 4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11) is: ");
 		
+		for(double i=1; i<12; i+=2){
+			answer1 += (1/i)*sign;
+			sign *= -1;
+		}
+		answer1 *= 4;
+		System.out.println(answer1);
+
 		//Calculate the answer to the second question: 4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11)
-		System.out.print("The answer to 4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13) is:  \n\n");
+		System.out.print("The answer to 4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13) is: ");
+
+		for(double i=1; i<14; i+=2){
+			answer2 += (1/i)*sign;
+			sign *= -1;
+		}
+		answer2 *= 4;
+		
+		System.out.println(answer2);
+		
+		sign = 1; //Reset sign to 1, because it was -1 before.
 		
 		//Calculate the answer to the extra credit portion
-		System.out.print("The answer to an expression using the first 1,000 terms of the above summation, rather than 6 or 7, is: \n\n");
+		System.out.print("The answer to an expression using the first 1,000 terms of the above summation, rather than 6 or 7, is: ");
+		for (double i=1; i<1001; i++){
+			answer3 += (1/denominator) * sign;
+			denominator += 2;
+			sign *= -1;
+		}
+		
+		answer3 *= 4;
+		
+		System.out.println(answer3);
 	}
 }
