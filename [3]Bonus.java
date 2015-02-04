153db1370adfac225fbf82e500f595b0003a7fd8
Chelesa Valentine <cv851@nyu.edu>
2015-02-03 22:06:08 -0500

    finish Bonus programming assignment

--- [3]Bonus.java
+++ [3]Bonus.java
@@ -17,6 +17,70 @@ import java.util.*;
  */
 public class Bonus {
 	public static void main(String[] args) {
-
+		
+		//Explain what this program does
+		System.out.println("This program will tell you the two employees who earned the highest bonus.\n");
+		
+		//Initialize variables
+		double employees, bonus, highestBonus, secondHighestBonus;
+		highestBonus = secondHighestBonus = 0;
+		String employee, highestEmployee, secondHighestEmployee;
+		employee = highestEmployee = secondHighestEmployee = "";
+		
+		//Ask the user how many employees they're entering data for
+		System.out.print("How many employees do you have? ");
+		Scanner in = new Scanner (System.in);
+		employees = in.nextInt();
+		
+		for (int i=1; i<=employees; i++){
+			
+			System.out.print("What is the name of employee #" + i + "? ");
+			employee = in.next();
+			
+			System.out.print("What was " + employee + "'s bonus? (omit the '$' sign) ");
+			bonus = in.nextDouble();
+			
+			if (bonus >= secondHighestBonus && bonus < highestBonus){
+				
+				//Take into account the possibility that 2 employees have the second highest bonus
+				if (bonus == secondHighestBonus){
+					secondHighestEmployee = secondHighestEmployee + " and " + employee;
+				}
+				
+				//If 2 employees don't share the second highest bonus, then just replace the 2nd highest completely.
+				else{
+					secondHighestBonus = bonus;
+					secondHighestEmployee = employee;
+				}
+			}
+			
+			else if (bonus >= highestBonus){
+				
+				//Take into account the possibility that 2 employees have the highest bonus
+				if (bonus == highestBonus){
+					highestEmployee = highestEmployee + " and " + employee;
+				}
+				
+				else{
+					//Make the current highestBonus be the secondHighestBonus
+					secondHighestBonus = highestBonus;
+					secondHighestEmployee = highestEmployee;
+					
+					//If 2 employees don't share the highest bonus, then just replace the highest bonus completely
+					highestBonus = bonus;
+					highestEmployee = employee;
+				}
+			}
+		}
+		
+		System.out.println("The highest bonus went to " + highestEmployee + " with $" + highestBonus + "\n");
+		
+		//Take into account that chance that there's only one employee
+		if (secondHighestEmployee != ""){
+			System.out.println("The second highest bonus went to " + secondHighestEmployee + " with $" + secondHighestBonus);
+		}
+		
+		
+		in.close();
 	}
 }
