ab30c65c3e60d49ba095ddb3e16016099953b6b3
Chelsea <cv851@nyu.edu>
2015-02-05 10:13:35 -0500

    Create [6]Sequence.java

---
+++ [6]Sequence.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/* @title: Interesting sequence
+ * @author: Chelsea Valentine
+ * @date: 
+ * 
+ * Assignment:
+ * 
+ * Write a program that reads as input from the user a single positive integer, call it n, and then uses n
+ * to compute the next number in the sequence as follows:
+ * 
+ * [*] if n is even, the next number n is divided by 2 (integer division)
+ * [*] if n is odd, the next number is n multiplied by 3 plus 1
+ * 
+ * The next number is computed based on the previous value and so on... For example, if n starts at 11, the
+ * sequence is 11, 34, 17, 52, 26, 13, 40, 20, 10, ...
+ * 
+ * This algorithm produces an infinite sequence of numbers. Compute the first hundred terms of the sequence
+ * and print the numbers using standard output. The numbers should be separated by a comma and a single space.
+ * Run your program several times using different input numbers. What do you notice about sequences? (Answer
+ * this question in the comments below the code in your program.)
+ * 
+ * Note: provide user interface that prompts the user for the input.
+ */
+
+public class Sequence {
+	public static void main(String[] args){
+		
+	}
+}
