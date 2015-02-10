0a7db6a2efbefe187e2671e426976443ee22e28e
Chelsea <cv851@nyu.edu>
2015-02-10 09:50:33 -0500

    Create [8]ReverseString.java

---
+++ [8]ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/* @title: Reverse String
+ * @author: Chelsea Valentine
+ * @date:
+ * 
+ * Assignment:
+ * 
+ * Write a program that prompts the user for a string and then prints out the same string in reverse. You
+ * should allow the user to enter spaces in the string. The user hits enter when they're done with the input.
+ * You're not allowed to use the method reverse() from the String class. Implement your own method that given
+ * a String object as a parameter, returns a String object that is a reverse of the parameter. Your method
+ * should have the following signature:
+ * 
+ * 		public static String reverse ( String textToReverse )
+ * 
+ * Hint: note the difference b/t next() & nextLine() Scanner methods
+ * Note: document your method as we discussed in class
+ */
+
+public class ReverseString {
+	public static void main (String[] args){
+		
+	}
+}
