ac67e53232aeb99bfe6d35fbc57584e64d761ee3
Chelesa Valentine <cv851@nyu.edu>
2015-02-17 01:31:09 -0500

    finish Poker Hand program

--- [7]GetPokerHand.java
+++ [7]GetPokerHand.java
@@ -16,7 +16,7 @@ import java.util.*;
  * 
  * Your program should print the names of the suit and the card "number" of all 5 randomly generated cards.
  * (eg. Your hand is:
- * 		9 of Spaces
+ * 		9 of Spades
  * 		...)
  * 
  * You don't need to worry about repetitions of a card.
@@ -32,5 +32,65 @@ import java.util.*;
 public class GetPokerHand {
 	public static void main (String[] args){
 		
+		//Let the user know what this program does
+		System.out.println("This program will 'randomly' generate 5 cards from a standard"
+				+ " deck of 52 cards.\n\n");
+		
+		//Let the user know what their hand is
+		System.out.println("Your hand is:");
+		cards(5);
+	}
+
+	public static void cards (int numberOfCards){
+		//Create the 4 arrays for hearts, clubs, spades, & diamonds
+		ArrayList<String> hearts = new ArrayList<>(13);
+		ArrayList<String> spades = new ArrayList<>(13);
+		ArrayList<String> clubs = new ArrayList<>(13);
+		ArrayList<String> diamonds = new ArrayList<>(13);
+		
+		addHighCards(hearts, "Hearts");
+		addHighCards(spades, "Spades");
+		addHighCards(clubs, "Clubs");
+		addHighCards(diamonds, "Diamonds");
+		
+		//Add cards 2 through 10 to each of the decks
+		for (int i = 2; i<=10; i++){
+			hearts.add(Integer.toString(i) + " of Hearts");
+			spades.add(Integer.toString(i) + " of Spades");
+			clubs.add(Integer.toString(i) + " of Clubs");
+			diamonds.add(Integer.toString(i) + " of Diamonds");
+		}
+		
+		//Add the all of the cards to a main stack of cards
+		List <String> mainStack = new ArrayList<String>();
+		
+		//Add cards of each suit to the main deck of cards
+		for (int i=0; i < 13; i++){
+			mainStack.add(hearts.get(i));
+			mainStack.add(spades.get(i));
+			mainStack.add(clubs.get(i));
+			mainStack.add(diamonds.get(i));
+		}
+
+		//Choose a random card out of the 52 cards; no repeats
+		Set<String> chosenCards = new HashSet<String>();
+		
+		while (chosenCards.size() != numberOfCards){
+			double chosenCard = Math.floor(Math.random() * 52);
+			String thisCard = mainStack.get((int)chosenCard);
+			chosenCards.add(thisCard);
+		}
+		
+		//Create an iterator to be able to display all elements of the set
+		Iterator<String> yourCardsAre = chosenCards.iterator();
+		while (yourCardsAre.hasNext())
+			System.out.println(yourCardsAre.next());
+	}
+	
+	public static void addHighCards(ArrayList<String> deck, String name){
+		deck.add("Ace of " + name);
+		deck.add("King of " + name);
+		deck.add("Queen of " + name);
+		deck.add("Jack of " + name);
 	}
 }
