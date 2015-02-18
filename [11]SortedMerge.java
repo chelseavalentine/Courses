8d72451d81fe4413c92324a007274c7ad5602b50
Chelsea <cv851@nyu.edu>
2015-02-18 06:58:55 -0500

    Create [11]SortedMerge.java

---
+++ [11]SortedMerge.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/*
+ * @title: Merge two lists
+ * @author: Chelsea Valentine
+ * @date:
+ * 
+ * Assignment:
+ * 
+ * Write a program that merges two lists of sorted whole numbers entered by the user. The program
+ * should use merge method with the following header:
+ * 		public static int [] merge (int [] list1, int [] list2)
+ * 
+ * The list returned by the method should contain all elements of the sorted list1 and sorted list2.
+ * The list returned by the method should be sorted. Your implementation should be as efficient as
+ * possible. Use the isSorted() method (that you developed in problem 1) to verify that both lists are
+ * sorted from smallest to largest & that your merged list is sorted.
+ * 
+ * Your program should ask the user for the length of each list and then read in the content of the
+ * two lists. Your program should print to the console the content of the merged list (numbers
+ * should be separated by commas and spaces).
+ * 
+ * Hint: Don't copy the 2 lists into a new array and sort the whole array. Take advantage of the
+ * fact that both list1 and list2 are in sorted order.
+ * 
+ * Note: Don't use Arrays or ArrayList classes.
+ */
+
+public class SortedMerge {
+	public static void main(String[] args){
+		
+	}
+}
