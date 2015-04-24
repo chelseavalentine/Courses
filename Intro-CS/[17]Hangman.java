7c7aa9cff2935f6569bb6bee7132d09dd5fae957
Chelsea <cv851@nyu.edu>
2015-04-24 17:00:00 -0400

    complete. time to comment, document, & 2x check

--- Intro-CS/[17]Hangman.java
+++ Intro-CS/[17]Hangman.java
@@ -29,28 +29,70 @@ public class Hangman {
 					"case", "point", "government", "company", "number", "group", "problem",
 					"fact"};
 			
-			//Keep track of whether the user has guessed the word correctly
-			boolean notGuessed = true;
-			
-			//This will be the word we use in the game
-			String word = chooseWord(words);
-			
-			//Convert that word into stars!
-			StringBuilder starBuilder = new StringBuilder(); 
-			for (int i = 0; i < word.length(); i++) {
-				starBuilder.append("*");
-			}
-			
-			String stars = starBuilder.toString();
-			
-			//Let's see what it is
-			System.out.println(word + " " + stars);
-			
 			//Let's allow user input!
 			Scanner in = new Scanner(System.in);
 			
-			if (notGuessed) {
-				System.out.println("(Guess) Enter a letter in word " + stars);
+			//Keep track of whether the user has guessed the word correctly & guess mistakes
+			boolean notGuessed = true;
+			int misses = 0;
+			boolean play = true;
+			
+			while (play) {
+				//This will be the word we use in the game
+				String word = chooseWord(words);
+				
+				//Convert that word into stars!
+				StringBuilder stars = new StringBuilder(); 
+				for (int i = 0; i < word.length(); i++) {
+					stars.append("*");
+				}
+				
+				String stringStars = stars.toString();
+								
+				//Let's see what it is
+				System.out.println(word + " " + stars);
+				
+				//Need to compare whether the user has guessed correctly, if they haven't,
+				//then we want to increment the amount of misses
+				String originalStars;
+				
+				while (notGuessed) {
+					System.out.println("(Guess) Enter a letter in word " + stars + " > ");
+					
+					//Take in the user's guess
+					originalStars = stringStars;
+					char guess = in.next().charAt(0);
+					
+					//Set the value of stars to the updated one
+					stringStars = correctChoice(stars, guess, word);
+					
+					//The user made an incorrect guess, so let's increment their misses
+					if (originalStars.equals(stars.toString())) {
+						misses += 1;
+					}
+					
+					//Let's check whether the user is done
+					if (stars.toString().equalsIgnoreCase(word)) {
+						notGuessed = false;
+					}
+				}
+				
+				//Show the user how well they did
+				System.out.println("The word is " + word + ". You missed " + misses + " time(s).\n");
+				
+				//Check whether the user wants to continue playing
+				System.out.println("Do you want to guess another word? Enter y or n > ");
+				char response = in.next().charAt(0);
+				
+				if (response == 'n') {
+					play = false;
+					System.out.println("\nThanks for playing! Bye bye~");
+				}
+				
+				else {
+					notGuessed = true;
+					misses = 0;
+				}
 			}
 			
 			in.close();
@@ -67,4 +109,15 @@ public class Hangman {
 			return word;
 		}
 		
+		
+		public static String correctChoice (StringBuilder stars, char guess, String word) {
+			
+			for (int i = 0; i < word.length(); i++) {
+				if (word.charAt(i) == guess) {
+					stars.replace(i, i+1, Character.toString(guess));
+				}
+			}		
+			
+			return stars.toString();
+		}
 }
