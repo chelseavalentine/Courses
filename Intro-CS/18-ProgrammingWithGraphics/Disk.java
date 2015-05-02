e621a666baac9bf9099891fb6a6ec20e7dc9c762
Chelsea <cv851@nyu.edu>
2015-05-02 00:52:53 -0400

    So done.

--- Intro-CS/18-ProgrammingWithGraphics/Disk.java
+++ Intro-CS/18-ProgrammingWithGraphics/Disk.java
@@ -2,6 +2,7 @@ package game;
 import processing.core.PApplet;
 
 public class Disk {
+	
 	//Create the variables that will be the disks' properties
 	protected int[] values = {10, 20, 50, 100}; //the values that a disk can have inside
 	protected float value; //the value the disk will take on
@@ -13,10 +14,10 @@ public class Disk {
 	protected float posX; //disk x position when generated
 	protected float posY; //disk y position when generated
 	protected int alive; //the time the disk has spent alive
-	protected String text;
-	protected Boolean hide = false;
-	PApplet canvas;
-	protected int textSize;
+	protected String text; //the text that'll go on the disk
+	protected Boolean hide = false; //should the disk be hidden?
+	PApplet canvas; //the canvas we're going to draw everything on
+	protected int textSize; //the size of the text that'll go on the disk
 	
 	//Create the default Disk object
 	public Disk ( PApplet canvas, Boolean hardMode) {
@@ -38,16 +39,16 @@ public class Disk {
 		dirX = canvas.random(-5, 5);
 		dirY = canvas.random(-5, 5);
 		
+		//if the user chose hard mode, let's give the user what they wanted
 		if (hardMode) {
 			size *= 0.5; //disks are smaller
 			speed *= 1.25; //and faster
 		}
 		
-		//generate the disks in random positions
+		//generate the disks in random positions & with a random color
 		posX = canvas.random(0, canvas.width);
 		posY = canvas.random(0, canvas.height);
-		
-		this.canvas.fill( canvas.random(20, 255), canvas.random(20, 255), canvas.random(20, 255));
+		this.canvas.fill( canvas.random(20, 255), canvas.random(20, 255), 255);
 		this.canvas.ellipse(posX, posY, size, size);
 		
 		//show the value of the disk
@@ -58,42 +59,53 @@ public class Disk {
 		this.canvas.text(text, posX, posY);
 	}
 		
+	/**
+	 * Give the disk life by moving it around at a certain speed.
+	 */
 	public void move() {
+		//Change the position so it's like it moved.
 		this.posX += this.dirX;
 		this.posY += this.dirY;
 		
 		// bouncing logic - see if the ball hits any of the edges
+		
+		//the disk hit the right wall, so reverse direction
 		if (posX > canvas.width) {
 			posX = canvas.width;
 			dirX *= -1;
 		}
 		
+		//disk hit the left wall, so reverse direction
 		if (posX < 0) {
 			posX = 0;
 			dirX *= -1; 
 		}
 		
+		//disk hit the floor, so reverse the direction
 		if (posY > canvas.height) {
 			posY = canvas.height;
 			dirY *= -1;
 		}
 		
+		//disk hit the ceiling, so reverse the direction
 		if (posY < 0) {
 			posY = 0;
 			dirY *= -1; 
 		}
 
+		//redraw the disk & give it a random blue-y color.
 		this.canvas.fill(canvas.random(10, 255), canvas.random(10, 255), 255);
 		this.canvas.ellipse(posX, posY, size, size);
+		
+		//rewrite the text
 		this.canvas.fill(255, 255, 255);
 		
+		//but if the disk is supposed to be hidden, let's hide it from view.
 		if (this.hide) {
 			this.canvas.text((int)this.value, -100, -100);
-		}
+		} 
 		else {
 			this.canvas.text((int)this.value, posX-15, posY+10);
 		}
 	}
-	
-	
 }
