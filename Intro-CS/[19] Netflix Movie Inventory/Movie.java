3277ffb1b55d317ebb77789e1a8b06eda69e4dee
Chelsea <cv851@nyu.edu>
2015-05-08 17:32:02 -0400

    done most of the movie class zzzz

--- Intro-CS/[19] Netflix Movie Inventory/Movie.java
+++ Intro-CS/[19] Netflix Movie Inventory/Movie.java
@@ -12,35 +12,10 @@ import java.io.*;
  * Write a program that keeps track of "Netflix"'s movie inventory. You need to design the following classes:
  * 
  * 
- * 
- * MOVIE: a single movie offered by Netflix. Has the following data fields:
- * - title (String): title of the movie
- * - yearReleased (int): year that the movie was released (and a positive integer between 1870 and 2015)
- * - rating (double): user rating (any positive floating number between 0.0 and 4.0)
- * - quantity (int): number of discs at the distribution center (a positive integer)
- * - duration (int): duration of the movie in minutes (a positive integer)
- * 
- * The class provides one constructor that takes 4 parameters (title, yearReleased, duration, rating) and sets
- * the quantity to 1;
- * 
- * The constructor should verify the parameters provided and throw an IllegalArgumentException exception if any
- * of the parameters are invalid.
- * 
- * The class needs to provide a way of modifying the quantity data field. The quantity should never fall below 0.
- * 
- * The class should provide a .toString() method that returns a String object in the following format:
- * 
- * TITLE (YEAR_RELEASED), rating: RATING, # in stock: QUANTITY
- * 
- * in which all the uppercase words are replaced by values of the corresponding data fields. Using the String.format(...)
- * method to format the string.
- * 
- * The class doesn't need any other getters & setters (but some of them may be useful). All data fields have to be private.
- * 
+ * MOVIE: a single movie offered by Netflix. 
  * 
  * {{assignment description pertaining to inventory is continued in Inventory.java}}
  *
- * 
  * 
  * THE PROGRAM: your program should take a single command line parameter. The parameter is the name of the file containing
  * the movie database. The program needs to read in the file and populate the inventory with the movies from the file. The
@@ -56,14 +31,38 @@ public class Movie {
 	private double rating;
 	private int quantity;
 	private int duration;
+	private ArrayList<Movie> list = new ArrayList<Movie>();
 	
 	public static void main (String[] args) {
 		try {
 			Scanner file = new Scanner (new File("movies_db.txt"));
 			
-			while (file.hasNextLine()) {
-				System.out.println(file.nextLine());
+			while (file.hasNext()) {
+				String[] entries = file.nextLine().split(" - ");
+				String newTitle = entries[0];
+				int newYear = Integer.parseInt(entries[1]);
+				int newDuration = Integer.parseInt(entries[2]);
+				double newRating = Double.parseDouble(entries[3]);
+				
+				try {
+					Movie fucker = new Movie (newTitle, newYear, newDuration, newRating);
+					System.out.println(fucker.toString());
+				}
+				
+				catch (IllegalArgumentException e) {
+					System.out.println("Uh oh. Something went wrong with one of the movies.");
+					System.out.println("Make sure that each movie adheres to the following "
+							+ "requirements: \n");
+					
+					System.out.println("The year the movie was released should be between"
+							+ " 1870 and 2015.");
+					System.out.println("The ratiing should be between 0.0 and 4.0");
+					System.out.println("The duration of the movie needs to be a positive integer.");
+				}
+				
+				new Movie (newTitle, newYear, newDuration, newRating);
 			}
+			
 			file.close();
 		}
 		
@@ -74,16 +73,39 @@ public class Movie {
 		}
 	}
 	
-	public Movie (String title, int yearReleased, double rating, int quantity, int duration) throws IllegalArgumentException {
+/*
+ * MOVIE: a single movie offered by Netflix. Has the following data fields:
+ * - quantity (int): number of discs at the distribution center (a positive integer)
+ */
+	public Movie (String title, int yearReleased, int duration, double rating) throws IllegalArgumentException {
 		this.title = title;
+		
+		if (yearReleased < 1870 | yearReleased > 2015) throw new IllegalArgumentException();
 		this.yearReleased = yearReleased;
+		
 		this.rating = rating;
-		this.quantity = quantity;
+		if (rating < 0.0 | rating > 4.0) throw new IllegalArgumentException();
+		
 		this.duration = duration;
+		if (duration < 0) throw new IllegalArgumentException();
+		
+		this.quantity = 1;
+	}
 	
+	
+	public void increaseQuantityBy1 () {
+		this.quantity++;
+	}
+	
+	public void decreaseQuantityBy1 () {
+		//Only decrement the quantity if the quantity won't drop below 0
+		if (this.quantity > 0) {
+			this.quantity--;
+		}
 	}
 	
 	public String toString() {
-		
+		String display = String.format("%s (%d), rating: %.1f, # in stock: %d", this.title, this.yearReleased, this.rating, this.quantity);
+		return display;
 	}
 }
