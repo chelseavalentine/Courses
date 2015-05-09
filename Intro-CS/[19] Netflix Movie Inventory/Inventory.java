a3a95d47202baf0381f0532c44ba316b5a2c69e1
Chelsea <cv851@nyu.edu>
2015-05-08 22:52:04 -0400

    Update Inventory.java

--- Intro-CS/[19] Netflix Movie Inventory/Inventory.java
+++ Intro-CS/[19] Netflix Movie Inventory/Inventory.java
@@ -12,40 +12,67 @@ import java.util.*;
  * This class should be based on an ArrayList of Movie objects.
  */
 
+
 public class Inventory {	
 	private ArrayList<Movie> list;
 	
+	/**
+	 * Create an Inventory object to hold all of our movies.
+	 */
 	public Inventory () {
 		this.list = new ArrayList<Movie>();
 	}
 	
+	/** Add a movie to the inventory.
+	 * @param title The title of the movie.
+	 * @param year The year the movie was released.
+	 * @param duration The length of the movie in minutes.
+	 * @param rating The rating of the movie.
+	 */
 	public void add(String title, int year, int duration, double rating) {
 		try {
+			// We first need to see if the movie doesn't exist, because we only want to add
+			// a new object if it doesn't already exist
 			boolean doesNotExist = true;
+			
+			// Check through all of the movies to see if it matches the parameters fed in
+			// to create a new Movie.
 			for (int i = 0; i < this.list.size(); i++) {
 				Movie checkThis = this.list.get(i);
-				if (checkThis.checkForRemoval(title, year)) {
+				
+				if (checkThis.checkMovie(title, year)) {
 					//It exists, so let's accurately reflect that
 					doesNotExist = false;
 					checkThis.increaseQuantityBy1();
 				}
 			}
 			
+			// If the movie really doesn't exist, let's go ahead and create our new movie
+			// then add it to our inventory.
 			if (doesNotExist) {
 				Movie addMovie = new Movie (title, year, duration, rating);
 				this.list.add(addMovie);
 			}
-		} catch (IllegalArgumentException e) {}
-		
+		} catch (IllegalArgumentException e) {}	
 	}
 	
+	
+	/** Remove a movie from the inventory.
+	 * @param title The title of the movie that we want to remove.
+	 * @param year The release year of the movie that we want to remove.
+	 */
 	public void remove(String title, int year) {
+		
+		// Check through the movies in our inventory to see if we have a movie with
+		// that release year and title
 		for (int i = 0; i < this.list.size(); i++) {
 			Movie checkThis = this.list.get(i);
-			if (checkThis.checkForRemoval(title, year)) {
-				//It exists so let's decrement the quantity.
+			
+			if (checkThis.checkMovie(title, year)) {
+				// It exists so let's decrement the quantity.
 				checkThis.decreaseQuantityBy1();
 				
+				// If the quantity of the movie is now 0, let's get rid of it all together.
 				if (checkThis.getQuantity() == 0) {
 					this.list.remove(this.list.get(i));
 				}
@@ -53,12 +80,17 @@ public class Inventory {
 		}
 	}
 	
+	
 	public String toString() {
+		// Create a StringBuilder to hold the list of all of the movies in the inventory
 		StringBuilder inventoryString = new StringBuilder();
+		
+		// Add each movie to the movie list one by one
 		for (int i = 0; i < this.list.size(); i++) {
 			Movie displayThis = this.list.get(i);
 			inventoryString.append(displayThis.toString() + "\n");
 		}
+		
 		return inventoryString.toString();
 	}
 }
