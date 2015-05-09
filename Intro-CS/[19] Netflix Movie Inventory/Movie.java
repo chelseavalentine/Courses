7eaba80febf4ebefcc552c829755e8ac842ef042
Chelsea <cv851@nyu.edu>
2015-05-08 22:52:19 -0400

    Update Movie.java

--- Intro-CS/[19] Netflix Movie Inventory/Movie.java
+++ Intro-CS/[19] Netflix Movie Inventory/Movie.java
@@ -9,8 +9,8 @@ import java.io.*;
  *  
  * @assignment:
  * 
- * Write a program that keeps track of "Netflix"'s movie inventory. You need to design the following classes:
- * 
+ * Write a program that keeps track of Netflix's movie inventory. You need to design the following classes:
+ *
  * 
  * MOVIE: a single movie offered by Netflix. 
  * 
@@ -34,13 +34,20 @@ public class Movie {
 	
 	
 	public static void main (String[] args) {
+		// Explain what the program does.
+		System.out.println("This program will take in a list of movies as input, and return"
+				+ " a clean inventory list.\n");
+		
+		// Make sure that the user knows how to use the program.
 		if (args.length > 0) {
 			try {
+				// Set up the movie list for input
 				Scanner file = new Scanner (new File(args[0]));
 				
-				//create new inventory
+				// create new inventory
 				Inventory movies = new Inventory();
 				
+				// Feed every line through the program, and create a Movie for each line.
 				while (file.hasNext()) {
 					String[] entries = file.nextLine().split(" - ");
 					String newTitle = entries[0];
@@ -48,10 +55,12 @@ public class Movie {
 					int newDuration = Integer.parseInt(entries[2]);
 					double newRating = Double.parseDouble(entries[3]);
 					
+					// Add movies to the list of movies.
 					try {
 						movies.add(newTitle, newYear, newDuration, newRating);
 					}
 					
+					// Explain why their movie wasn't created.
 					catch (IllegalArgumentException e) {
 						System.out.println("Uh oh. Something went wrong with one of the movies.");
 						System.out.println("Make sure that each movie adheres to the following "
@@ -64,6 +73,7 @@ public class Movie {
 					}
 				}
 				
+				// Show the inventory list.
 				System.out.println(movies.toString());
 				
 				file.close();
@@ -74,51 +84,82 @@ public class Movie {
 						+ " the program expected it to be.\n\nPlease put it where your"
 						+ " src folder is.");
 			}
+			
 		} else {
 			System.out.println("Yo. You need to pass the name of the .txt file as a parameter.");
-		}
-		
+		}	
 	}
 	
+	
+	/** Create a Movie object, that represents a movie we have in stock.
+	 * @param title The title of the movie.
+	 * @param yearReleased The year the movie was released.
+	 * @param duration The length of the movie in minutes.
+	 * @param rating The rating of the movie.
+	 * @throws IllegalArgumentException If the user tries to make a movie that doesn't fit certain criteria, this exception is thrown.
+	 */
 	public Movie (String title, int yearReleased, int duration, double rating) throws IllegalArgumentException {
 		this.title = title;
 		
+		// Ensure that the movie was released before 2015 and after 1870.
 		if (yearReleased < 1870 | yearReleased > 2015) throw new IllegalArgumentException();
 		this.yearReleased = yearReleased;
 		
-		this.rating = rating;
+		// Ensure that the rating is between 0.0 and 4.0.
 		if (rating < 0.0 | rating > 4.0) throw new IllegalArgumentException();
+		this.rating = rating;
 		
-		this.duration = duration;
+		// Ensure that the duration is a positive integer.
 		if (duration < 0) throw new IllegalArgumentException();
+		this.duration = duration;
 		
 		this.quantity = 1;
 	}
 	
 	
+	/**
+	 * Increase the quantity of a particular movie by 1.
+	 */
 	public void increaseQuantityBy1 () {
 		this.quantity++;
 	}
 	
+	
+	/** 
+	 * Decrease the quantity of a particular movie by 1.
+	 */
 	public void decreaseQuantityBy1 () {
-		//Only decrement the quantity if the quantity won't drop below 0
+		// Only decrement the quantity if the quantity won't drop below 0
 		if (this.quantity > 0) {
 			this.quantity--;
 		}
 	}
 	
+	
+	/** Get the quantity of a particular movie.
+	 * @return the amount of a movie that's in stock
+	 */
 	public int getQuantity() {
 		return this.quantity;
 	}
 	
-	public boolean checkForRemoval (String title, int year) {
+	
+	/** Find whether a movie already exists in our stock, for deletion or addition to inventory purposes.
+	 * @param title The title of the movie that we're looking for.
+	 * @param year The year that the movie we're looking for was released.
+	 * @return Whether the movie matches the comparison parameters.
+	 */
+	public boolean checkMovie (String title, int year) {
+		
+		// If the movie does exist, return true
 		if (this.title == title && this.yearReleased == year) {
 			return true;
-		}
-		else {
+		} else {
+			// Otherwise, the movie doesn't exist, so return false
 			return false;
 		}
 	}
+	
 	
 	public String toString() {
 		String display = String.format("%s (%d), rating: %.1f, # in stock: %d", this.title, this.yearReleased, this.rating, this.quantity);
