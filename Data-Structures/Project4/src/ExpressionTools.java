2e7b47d9550548572caf3b0b0ce9925b4acc997c
Chelsea Valentine <chelseafrv@gmail.com>
2015-11-25 22:40:21 -0500

    write first iter of expression tools

--- Data-Structures/Project4/src/ExpressionTools.java
+++ Data-Structures/Project4/src/ExpressionTools.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * ExpressionTools
@@ -13,98 +14,192 @@ public class ExpressionTools {
     private ExpressionTools() {}
 
     /**
-     * 
+     * Converts an infix expression, such as 23 - 2 * 5 to
      * @throws PostFixException
      */
-    public static void convertInfixToPostfix(String expression) throws PostFixException {
-        MyStack<Integer> operandStack = new MyStack<Integer>();
-        MyStack<String> operatorStack = new MyStack<String>();
+    public static String convertInfixToPostfix(String infix) throws PostFixException {
+        Stack<Character> operatorStack = new Stack<>();
 
-        // Create a scanner to look through the equation
-        Scanner expressionReader = new Scanner(expression);
+        // Create a scanner to look through the infix expression
+        Scanner expressionReader = new Scanner(infix);
         StringBuilder postfix = new StringBuilder();
         String token;
-        int operand;
 
+        int run = 0;
         // for each token in the input infix expression
         while (expressionReader.hasNext()) {
+            run++;
             token = expressionReader.next();
+            System.out.println("token is " + token);
 
-            // check whether the token is one of the operators
-            // if it is, add it to the operator stack
-            if(token.equals("*") || token.equals("/") || token.equals("+") || token.equals("-"))
-                operatorStack.push(token);
+            // the token is an operand (ie. an integer), add it to the postfix string expression
+            if (isOperand(token)) {
+                postfix.append(token + " ");
+            }
 
-            // if the token is a left brace
-            else if(token.equals("("))
-                operatorStack.push(token);
+            // the token is a left brace, then push it onto the operator stack
+            else if (token.equals("(")) {
+                operatorStack.push(token.charAt(0));
+            }
 
-            // if the token is a right brace
-            else if(token.equals("(")) {
-                // while the operator stack isn't empty
-                while (!operatorStack.empty()) {
-                    // if the top of the operator stack isn't a matching left brace
-                    if(higherPrecedence(operatorStack.peek().charAt(0), token.charAt(0))) {
-                        // pop the operator stack and append to the postfix string expression
+            // the token is an operator
+            else if(token.equals("*") || token.equals("/") || token.equals("+") || token.equals("-")) {
+                if (!operatorStack.empty()) {
+                    // while the top stack element has higher or equal precedence, pop the operator stack and append the
+                    // operator to the postfix expression
+                    while (!(operatorStack.empty()) && higherOrEqualPrecedence(token.charAt(0), operatorStack.peek())) {
+                        System.out.println("peeking  @ " + operatorStack.peek());
+                        postfix.append(operatorStack.pop() + " ");
+                        System.out.println("now the postfox is " + postfix);
                     }
-                    // else
-                    else {
-                        // pop the left brace and discard
+                }
+                // push the current operator onto the operator stack
+                operatorStack.push(token.charAt(0));
+            }
+
+            // the token is a right brace
+            else if (token.equals(")")) {
+                System.out.println("token is )");
+                while (!operatorStack.empty()) {
+                    // if the top of the operator stack isn't a matching left brace, pop the operator stack and append
+                    // it to the postfix expression
+                    if (operatorStack.peek() != '(') {
+                        System.out.println("It's an operator and it's not (");
+                        postfix.append(operatorStack.pop() + " ");
+                        System.out.println("postfix is now " + postfix.toString());
+                    } else {
+                        // if it's a left brace, pop it and discard it
+                        operatorStack.pop();
                         break;
                     }
                 }
-                operatorStack.push(token);
             }
-            // the token isn't an operator, nor a brace, then it's either an integer--which we
-            // want--or a letter, which we don't want
+            // the token isn't an operator, brace, or integer, so it's some other character that we cannot use in
+            // calculations
             else {
-                for (int i = 0; i < token.length(); i++) {
-                    char character = token.charAt(i);
-                    // If the character is a digit, or the character is the first character & a negative or positive
-                    // sign, then do nothing
-                    if (Character.isDigit(character) || ((character == '-' || character == '+') && i == 0));
-                    // Else, we don't have an integer and we have an invalid expression
-                    else break;
-                    // TODO: print invalid
-                }
-
-                // Parse the integer to add to our stack
-                operand = Integer.parseInt(token);
-                operandStack.push(operand);
+                throw new PostFixException(infix + " is not a valid infix expression because it contains token " + token);
             }
-
-               // append to postfix string expression
-            // else if the token is a left brace
-                // push it onto the operator stack
-            // else if the token is an operator
-                // if the operator stack is not empty
-                    // while the top element on the stack has precedence higher or equal
-                        // pop the stack and append to postfix string expression
-                // push it (the current operator) onto the operator stack
-            // else if the token is a right brace
-                // while the operator stack is not empty
-                    //if the top of the operator stack is not a matching left brace
-                        // pop the operator stack and append to postfix string expression
-                    // else
-                        // pop the left brace and discard
-                        // break
-            // while the operator stack is not empty
-                // pop the operator stack and append it to the postfix string operation
         }
+
+        // while the operator stack isn't empty, add each character to the postfix expression one by one
+        while (!operatorStack.empty()) {
+            postfix.append(operatorStack.pop() + " ");
+        }
+
+        return postfix.toString();
     }
-    private static boolean higherPrecedence(char operator1, char operator2) {
+
+    /**
+     * Evaluates whether the second operator has higher or equal precedence than the first operator, based on PEMDAS
+     * (Parentheses > Exponent > (Multiplication | Division) > (Addition | Subtraction).
+     * @param operator1 first operator compared against
+     * @param operator2 what the first operator is being compared to
+     * @return whether operator 2 has higher or equal precedence than operator 1
+     */
+    private static boolean higherOrEqualPrecedence(char operator1, char operator2) {
+        System.out.println("comparing " + operator1 + " and " + operator2);
+        switch (operator1) {
+            // We need to deal with the parentheses too because they're also pushed onto the operator stack
+            case '(':
+                return true;
+            // The '*' case is empty because it will fall to the '/' case when operator is '*'. Multiplication and
+            // division have equal precedence, so this is fine
+            case '*':
+            case '/':
+                return (operator2 == '*' || operator2 == '/');
+
+            // The '+' case is empty because it will fall to the '-' case when operator is '+'. Addition and subtraction
+            // have equal precedence, so this is fine
+            case '+':
+            case '-':
+                return (operator2 == '*' || operator2 == '/' || operator2 == '+' || operator2 == '-');
+
+        }
         return false;
     }
-    public static void evaluatePostfix() {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(convertInfixToPostfix("( 300 + 23 ) * ( 43 - 21 ) / ( 84 + 7 )"));
+        } catch (PostFixException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Evaluate a postfix expression
+     * @param postfix the postfix expression
+     * @return the computation result of the expression
+     */
+    public static int evaluatePostfix(String postfix) {
+        // Create a scanner to look through the infix expression
+        Scanner reader = new Scanner(postfix);
+        int answer = 0;
+        Stack<Character> operatorStack = new Stack<>();
+        Stack<Integer> operandStack = new Stack<>();
+        int operator1, operator2, result;
+
+        String token;
         // scan the given postfix expression from left to right
-        // for each token in the input postfix expression
-            // if the token is an operand
-                // push it (its value) onto a stack
-            // else if the token is an operator
-                // operand2 = pop stack
-                // operand1 = popstack
+        while (reader.hasNext()) {
+            token = reader.next();
+
+            // the token is an operand
+            if (isOperand(token)) {
+                operandStack.push(Integer.parseInt(token));
+            }
+
+            // the token is an operator
+            else if (isOperator(token)) {
                 // compute operand1 operator operand2
-                // push result onto stack
-        // return top of the stack as a result
+                result = computeResult(operandStack.pop(), token.charAt(0), operandStack.pop());
+
+                // push the result unto the stack
+                operandStack.push(result);
+            }
+        }
+        // return the top of the stack as the result
+        return operandStack.pop();
+    }
+
+    /**
+     * Compute the result of 2 operands and an operator
+     * @param operand1 the first integer
+     * @param operator the operator
+     * @param operand2 the second integer
+     * @return the answer to operand1 operator operand2
+     */
+    private static int computeResult(int operand1, char operator, int operand2) {
+        switch (operator) {
+            case '*':
+                return operand1 * operand2;
+            case '/':
+                return operand1 / operand2;
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 - operand2;
+        }
+        return 0;
+    }
+
+    /**
+     * Determine whether the token is an operand
+     * @param token a string
+     * @return whether the token is an operand (aka an integer)
+     */
+    private static boolean isOperand(String token) {
+        return token.matches("\\d*") || (token.charAt(0) == '-' && token.substring(1, token.length()).matches("\\d*"));
+    }
+
+    /**
+     * Determine whether the token is an operator
+     * @param token a string
+     * @return whether the token is an operator *, /, +, or -
+     */
+    private static boolean isOperator(String token) {
+        // if the token isn't one character, it's definitely not an operator, so return false
+        if (token.length() != 1) return false;
+        return token.charAt(0) == '*' || token.charAt(0) == '/' || token.charAt(0) == '+' || token.charAt(0) == '-';
     }
 }
